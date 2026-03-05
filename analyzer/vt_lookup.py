c3b679f63103f38e23d6114b79d138bec71a086a
Jamir Banks <jrule7220@gmail.com>
2026-03-05 12:41:20 -0500

    Update vt_lookup.py

--- analyzer/vt_lookup.py
+++ analyzer/vt_lookup.py
@@ -1,1 +1,19 @@
+import requests
 
+API_KEY = "YOUR_VT_API_KEY"
+
+def check_domain(domain):
+
+    url = f"https://www.virustotal.com/api/v3/domains/{domain}"
+
+    headers = {
+        "x-apikey": API_KEY
+    }
+
+    response = requests.get(url, headers=headers)
+
+    data = response.json()
+
+    stats = data["data"]["attributes"]["last_analysis_stats"]
+
+    return stats
