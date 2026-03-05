ea6f2cfe75d755a1d6cffac17380ecceff279af1
Jamir Banks <jrule7220@gmail.com>
2026-03-05 12:41:07 -0500

    Update whois_lookup.py

--- analyzer/whois_lookup.py
+++ analyzer/whois_lookup.py
@@ -1,1 +1,17 @@
+import whois
+from datetime import datetime
 
+def check_domain_age(domain):
+    data = whois.whois(domain)
+
+    creation_date = data.creation_date
+
+    if isinstance(creation_date, list):
+        creation_date = creation_date[0]
+
+    age_days = (datetime.now() - creation_date).days
+
+    return {
+        "creation_date": str(creation_date),
+        "age_days": age_days
+    }
