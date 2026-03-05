c4d0be72be0a956b7fe6c371d025f3873db7b46a
Jamir Banks <jrule7220@gmail.com>
2026-03-05 12:41:45 -0500

    Update scraper.py

--- analyzer/scraper.py
+++ analyzer/scraper.py
@@ -1,1 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
 
+def fetch_page(domain):
+
+    url = f"http://{domain}"
+
+    r = requests.get(url, timeout=10)
+
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    text = soup.get_text()
+
+    forms = soup.find_all("form")
+
+    return {
+        "text_sample": text[:2000],
+        "forms_detected": len(forms)
+    }
