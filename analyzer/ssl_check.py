80907f2f6c84396cc3875cdb28feab4894e21d90
Jamir Banks <jrule7220@gmail.com>
2026-03-05 12:41:31 -0500

    Update ssl_check.py

--- analyzer/ssl_check.py
+++ analyzer/ssl_check.py
@@ -1,1 +1,12 @@
+import ssl
+import socket
 
+def get_ssl_info(domain):
+
+    ctx = ssl.create_default_context()
+
+    with socket.create_connection((domain, 443)) as sock:
+        with ctx.wrap_socket(sock, server_hostname=domain) as ssock:
+            cert = ssock.getpeercert()
+
+    return cert
